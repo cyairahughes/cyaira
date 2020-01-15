84f6ffe21adc03b3a5f1aeb7c979b1bb2d6b5443
cyairahughes <59940992+cyairahughes@users.noreply.github.com>
2020-01-15 18:25:16 -0500

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,20 @@
+/**********************************************************
+ *Hello.Java
+ **********************************************************
+ *Cyaira Hughes
+ *1/15/20
+ *255-901
+ *********************************************************/
+ 
+ public class hello{
+	 
+	 public static void main(String[] args){
+	 
+	 System.out.println("<Cyaira Hughes>");
+	 System.out.println("<Hello.Java>");
+	 System.out.println("<Lab 1>");
+	 System.out.println("<1/15/20>");
+	 System.out.println("<225-901>");
+	 System.out.println();
+	 }
+ }
